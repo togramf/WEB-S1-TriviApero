1769040561f88cfad5d41b21a00c98225b99f243
Margot <togramf2@gmail.com>
2020-12-17 11:46:07 +0100

    JS : récup et affichage d'une question
    choix difficulté : OK 
    choix catégorie : en cours 
    affichage réponses : en cours

--- main.js
+++ main.js
@@ -8,11 +8,14 @@ const liste_participants = document.querySelector('.list_players');
 const btn_add_player = document.querySelector('#add_pseudo');
 const btn_start = document.querySelector('#start_game');
 const btn_play = document.querySelector('#play_game');
+const btn_question = document.querySelector('#start_question');
 const nbPlayers = document.querySelector('#nbPlayersValueId');
 const slide_nbPlayers = document.querySelector('#nbPlayersId');
 const categories = document.querySelector('#categories');
+const questions = document.querySelector('#question_area');
+const answers = document.querySelector('#answer_area');
 
-const url_API = 'https://opentdb.com/api.php?amount=10&token=Triviapero';
+//const url_API = 'https://opentdb.com/api.php?amount=10&token=Triviapero';
 
 function displayOrHide (element){
     element.classList.toggle("hidden_element");
@@ -21,7 +24,7 @@ function displayOrHide (element){
 function startClick(e){
     console.log("Lancement du Jeu !");
     displayOrHide(section_accueil);
-    displayOrHide(section_parametres)
+    displayOrHide(section_parametres);
 }
 
 btn_start.addEventListener('click',startClick);
@@ -49,11 +52,10 @@ btn_add_player.addEventListener('click', addPlayer);
 function playClick(e){
     console.log("Lancement du Jeu !");
     displayOrHide(section_parametres);
-    displayOrHide(section_jeu)
+    displayOrHide(section_jeu);
 }
 
 btn_play.addEventListener('click',playClick);
-
 
 
 async function getCategories(){
@@ -62,14 +64,15 @@ async function getCategories(){
 
     try {
         //console.log(body.trivia_categories);
+        console.log(body);
         for (i=0; i<body.trivia_categories.length; i++){
-            console.log(body.trivia_categories[i].id);
-            console.log(body.trivia_categories[i].name);
+            //console.log(body.trivia_categories[i].id);
+            //console.log(body.trivia_categories[i].name);
             
             var newElement = document.createElement('button');
             newElement.type = "button";
             //newElement.id=body.trivia_categories[i].id;
-            newElement.name=body.trivia_categories[i].name;
+            //newElement.name=body.trivia_categories[i].name;
             newElement.textContent=body.trivia_categories[i].name;
             newElement.classList.add("category");
             newElement.addEventListener('click',categoriesClick,false);
@@ -85,3 +88,54 @@ btn_play.addEventListener('click', getCategories);
 function categoriesClick (e){
     console.log(e.currentTarget.name);
 }
+
+async function getQuestion(){
+    //var category =  ;
+    var difficulty = document.querySelector('#select_difficulty').value;
+    const response = await fetch('https://opentdb.com/api.php?amount=1&category=9&difficulty='+difficulty);
+    const body = await response.json();
+
+    try {
+        console.log(body);
+        createQuestion(body.results[0]);
+    }
+    catch {
+        console.log("erreur");
+    }
+}
+
+function createQuestion ( quest ) {
+    console.log(quest.category);
+    var newQuestion = document.createElement('div');
+    newQuestion.classList.add("question");
+    newQuestion.classList.add(quest.type);
+    var newQuestCat = document.createElement('p');
+    newQuestCat.textContent = quest.category;
+    var newQuestDiff = document.createElement('p');
+    newQuestDiff.textContent = quest.difficulty;
+    var newQuest = document.createElement('p');
+    newQuest.textContent = quest.question;
+    newQuestion.appendChild(newQuestCat);
+    newQuestion.appendChild(newQuestDiff);
+    newQuestion.appendChild(newQuest);
+    questions.appendChild(newQuestion);
+
+    var newAnswer = document.createElement('div');
+    newAnswer.classList.add("answer");
+    if (quest.type == "multiple" || quest.type == "boolean"){
+        for (var i = 0; i < quest.incorrect_answers.length; i++){
+            var newProposition = document.createElement('p');
+            newProposition.classList.add("incorrect_answer");
+            newProposition.textContent = quest.incorrect_answers[i];
+            newAnswer.appendChild(newProposition);
+        }
+        //insérer la bonne réponse à une position aléatoire de newAnswer
+        var newProposition = document.createElement('p');
+        newProposition.classList.add("correct_answer");
+        newProposition.textContent = quest.correct_answer;
+        newAnswer.appendChild(newProposition);
+    }
+    answers.appendChild(newAnswer);
+}
+
+btn_question.addEventListener('click', getQuestion);
