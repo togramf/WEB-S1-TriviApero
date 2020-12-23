e195a44cf6c3ae55a1e554293555580c4f68bb50
Margot <togramf2@gmail.com>
2020-12-23 19:26:05 +0100

    HTML js et CSS : merge des 2 versions + bonus
    propreté du code + système de score + système de round + aspect graphique

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 /* rappel : #id et .class */
 
-const section_accueil = document.querySelector('#accueil');
-const section_parametres = document.querySelector('#parametre_partie');
-const section_jeu = document.querySelector('#jeu');
-const participants = document.querySelector('#players');
-const liste_participants = document.querySelector('.list_players');
-const btn_add_player = document.querySelector('#add_pseudo');
-const btn_start = document.querySelector('#start_game');
-const btn_play = document.querySelector('#play_game');
-const btn_question = document.querySelector('#start_question');
-const nbPlayers = document.querySelector('#nbPlayersValueId');
-const slide_nbPlayers = document.querySelector('#nbPlayersId');
+const section_menu = document.querySelector('#menu_section');
+const section_parametres = document.querySelector('#param_section');
+const section_jeu = document.querySelector('#game_section');
+const section_scores = document.querySelector('#score_section');
+
+const game_menu = document.querySelector('#game_menu');
+const play_menu = document.querySelector('#play_menu');
+const play_area = document.querySelector('#play_area');
+const result_area = document.querySelector('#result_area');
+
 const categories = document.querySelector('#select_category');
 const questions = document.querySelector('#question_area');
 const answers = document.querySelector('#answer_area');
+const result = document.querySelector('#result');
+const scores_area = document.querySelector('#scores_area');
 
-//const url_API = 'https://opentdb.com/api.php?amount=10&token=Triviapero';
+const liste_players = document.querySelector('.list_players');
+const nbPlayers = document.querySelector('#nbPlayersValueId');
+const slide_nbPlayers = document.querySelector('#nbPlayersId');
+
+const btn_start = document.querySelector('#start_game');
+const btn_play = document.querySelector('#play_game');
+const btn_question = document.querySelector('#start_question');
+const btn_next = document.querySelector('#next_game');
+
+const btn_add_player = document.querySelector('#add_pseudo');
+
+const stop_game = false; 
+var current_player = 1;
+var current_round = 1;
+
 
 function displayOrHide (element){
     element.classList.toggle("hidden_element");
@@ -23,38 +38,54 @@ function displayOrHide (element){
 
 function startClick(e){
     console.log("Lancement du Jeu !");
-    displayOrHide(section_accueil);
+    displayOrHide(section_menu); 
     displayOrHide(section_parametres);
 }
-
 btn_start.addEventListener('click',startClick);
 
-const addPlayer = () => {
-    var pseudo = document.querySelector('#pseudo').value;
+
+function addPlayer() {
     var player = document.createElement('li');
-    player.textContent=pseudo;
-    player.value=pseudo;
-    liste_participants.appendChild(player);
+    
+    player.textContent = document.querySelector('#pseudo').value;
+    player.value = 0; //sert a compter le score 
+
+    liste_players.appendChild(player);
+
     slide_nbPlayers.value++;
     nbPlayers.value = slide_nbPlayers.value;
-    console.log("Ajout de ", pseudo);
 
-    if (slide_nbPlayers.value==2){
+    player_score = document.createElement('li');
+    player_score.value = player.value;
+    player_score.textContent = "Score de "+player.textContent+" = "+player.value;
+    
+    scores_area.appendChild(player_score);
+    
+    console.log("Ajout de ", player.textContent);
+
+    if (slide_nbPlayers.value == 2) {
         displayOrHide(btn_play);
     }
-    if (slide_nbPlayers.value==10){
+    if (slide_nbPlayers.value == 10) {
         displayOrHide(document.querySelector('.management-form'));
     }
 }
-
 btn_add_player.addEventListener('click', addPlayer);
+
+const players = liste_players.childNodes;
+const scores = scores_area.childNodes;
 
 function playClick(e){
     console.log("Lancement du Jeu !");
     displayOrHide(section_parametres);
+    players[current_player].classList.toggle("current_player");
+    document.querySelector('#current_player').textContent = "Tour de "+players[current_player].textContent;
+    document.querySelector('#current_player').value = current_player;
     displayOrHide(section_jeu);
-}
+    displayOrHide(section_scores);
 
+    getCategories();
+}
 btn_play.addEventListener('click',playClick);
 
 
@@ -63,15 +94,11 @@ async function getCategories(){
     const body = await response.json();
 
     try {
-        //console.log(body.trivia_categories);
         console.log(body);
         for (i=0; i<body.trivia_categories.length; i++){
-            //console.log(body.trivia_categories[i].id);
-            //console.log(body.trivia_categories[i].name);
+            
             
             var newElement = document.createElement('option');
-            //newElement.id=body.trivia_categories[i].id;
-            //newElement.name=body.trivia_categories[i].name;
             newElement.value = body.trivia_categories[i].id;
             newElement.textContent=body.trivia_categories[i].name;
             categories.appendChild(newElement);
@@ -81,12 +108,18 @@ async function getCategories(){
         console.log("erreur");
     }
 }
-btn_play.addEventListener('click', getCategories);
 
 async function getQuestion(){
+    var url;
     var cat = document.querySelector('#select_category').value;
     var diff = document.querySelector('#select_difficulty').value;
-    const response = await fetch('https://opentdb.com/api.php?amount=1&category='+cat+'&difficulty='+diff);
+    console.log(cat, diff);
+    if (diff != "any_difficulty"){
+        url = 'https://opentdb.com/api.php?amount=1&category='+cat+'&difficulty='+diff;
+    } else {
+        url = 'https://opentdb.com/api.php?amount=1&category='+cat;
+    }
+    const response = await fetch(url);
     const body = await response.json();
 
     try {
@@ -96,55 +129,88 @@ async function getQuestion(){
     catch {
         console.log("erreur");
     }
+    displayOrHide(play_area);
+    displayOrHide(play_menu);
 }
 
 function createQuestion ( quest ) {
     console.log(quest.category);
-    /*VERSION 1 
-    //supprime la question precedente
-    var newQuestion = document.createElement('div');
-    newQuestion.classList.add("question");
-    newQuestion.classList.add(quest.type);
-    var newQuestCat = document.createElement('p');
-    newQuestCat.textContent = quest.category;
-    var newQuestDiff = document.createElement('p');
-    newQuestDiff.textContent = quest.difficulty;
-    var newQuest = document.createElement('p');
-    newQuest.textContent = quest.question;
-    newQuestion.appendChild(newQuestCat);
-    newQuestion.appendChild(newQuestDiff);
-    newQuestion.appendChild(newQuest);
-    questions.appendChild(newQuestion);
 
-    var newAnswer = document.createElement('div');
-    newAnswer.classList.add("answer");
-    if (quest.type == "multiple" || quest.type == "boolean"){
-        for (var i = 0; i < quest.incorrect_answers.length; i++){
-            var newProposition = document.createElement('p');
-            newProposition.classList.add("incorrect_answer");
-            newProposition.textContent = quest.incorrect_answers[i];
-            newAnswer.appendChild(newProposition);
-        }
-        //insérer la bonne réponse à une position aléatoire de newAnswer
-        var newProposition = document.createElement('p');
-        newProposition.classList.add("correct_answer");
-        newProposition.textContent = quest.correct_answer;
-        newAnswer.appendChild(newProposition);
-    }
-    answers.appendChild(newAnswer);
-    */
-
-    /*VERSION 2 */
-    // remplace la question precedente 
     document.querySelector('#question_category').textContent = quest.category;
     document.querySelector('#question_difficulty').textContent = quest.difficulty;
     document.querySelector('#question_wording').textContent = quest.question;
 
-    document.querySelector('.correct_answer').textContent = quest.correct_answer;
-    //provisoirement on va faire comme ca
-    document.querySelector('.incorrect_answer').textContent = quest.incorrect_answers;
-    
+    while (answers.lastChild){
+        answers.removeChild(answers.lastChild);
+    }
 
+    var nb_options = 1 + quest.incorrect_answers.length; 
+    var position_correct = Math.round((nb_options -1) * Math.random()) + 1;
+    console.log(nb_options, position_correct);
+
+    var j = 0;
+    for (var i = 1; i<=nb_options; i++){
+        var newProposition = document.createElement('p');
+        if (i!=position_correct){
+            newProposition.classList.add("incorrect_answer");
+            newProposition.textContent = quest.incorrect_answers[j];
+            j ++;
+        } else {
+            newProposition.classList.add("correct_answer");
+            newProposition.textContent = quest.correct_answer;
+        }
+        newProposition.addEventListener('click', submitAnswer);
+        answers.appendChild(newProposition);
+    }    
+}
+btn_question.addEventListener('click', getQuestion);
+
+
+function submitAnswer ( e ){
+    displayOrHide(play_area);
+    
+    var diff = document.querySelector('#question_difficulty');
+    var score; 
+    if (diff == 'easy'){
+        score = 1;
+    } else if (diff ='medium'){
+        score = 2;
+    } else if (diff = 'hard'){
+        score = 3;
+    }
+
+    if (e.currentTarget.classList.contains('correct_answer')){
+        console.log("Bonne réponse ! ");
+        players[current_player].value += 5*score;
+        result.textContent = 'Bravo, bonne réponse '+players[current_player].textContent+' ! '+5*score+' points en plus !';
+    } else {
+        console.log("Mauvaise réponse ! ");
+        players[current_player].value -= 5*score;
+        result.textContent = 'Dommage, mauvaise réponse '+players[current_player].textContent+' ! '+5*score+' points en moins !';
+    }
+    scores[current_player-1].textContent="Score de "+players[current_player].textContent+" = "+players[current_player].value;
+    displayOrHide(result_area);
 }
 
-btn_question.addEventListener('click', getQuestion);
+function nextClick (e) {
+    players[current_player].classList.toggle("current_player");
+    if (current_player<nbPlayers.textContent){
+        current_player += 1;
+    } else {
+        current_player = 1;
+        current_round += 1;
+        document.querySelector('#round_compteur').textContent = 'Round '+current_round;
+    }
+    
+    players[current_player].classList.toggle("current_player");
+
+    console.log(players[current_player-1]);
+    console.log(players[current_player]);
+
+    document.querySelector('#current_player').textContent = "Tour de "+players[current_player].textContent;
+    document.querySelector('#current_player').value = current_player;
+    
+    displayOrHide(play_menu);
+    displayOrHide(result_area);
+}
+btn_next.addEventListener('click',nextClick);
